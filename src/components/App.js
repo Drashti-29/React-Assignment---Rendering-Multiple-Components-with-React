2d5c020cfeefec02473bb81479c796d48f1e25f8
Drashti <72065999+Drashti-29@users.noreply.github.com>
2020-11-20 14:38:48 +0530

    Rendering Multiple Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
-const App = () => {
-  return (
-    <div id="main"></div>
-  )
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1 data-ns-test="project-name"> MovieList</h1>
+        <div data-ns-test="project-description">
+          This web app provides the movie list and if You want to like you can
+          like it, add to watchlist or you can remove it from MovieList
+        </div>
+      </>
+    );
+  }
 }
-
 
 export default App;
